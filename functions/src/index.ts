bfaf7f6bfd66ac364733662b46a11c7ca408004b
bruslaan <bruslan.no@gmail.com>
2020-09-11 03:05:18 +0200

    stripe added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// // // Start writing Firebase Functions
+// // // https://firebase.google.com/docs/functions/typescript
+// //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const stripe = require('stripe')('sk_test_51H4wHkAp105LrKrukopj6dzbqJqJg1460nzZPdgeKo8Dl9RnqZy4C3mZNtOvocVb5eVyoNTDfK2390JkW96Gk8Jw004zzRTgdF');
+const express = require('express');
+const cors = require("cors")
+const app = express();
+app.use(cors({ origin: true }))
+
+app.get('/secret', async (req: any, res: any) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 2999,
+    currency: 'eur',
+    // Verify your integration in this guide by including this parameter
+    metadata: { integration_check: 'accept_a_payment' },
+  });
+  res.json({ client_secret: paymentIntent.client_secret });
+});
+
+app.get('/hallo', async (req: any, res: any) => {
+
+  res.json({ "hallo": "World" });
+});
+
+
+exports.api = functions.https.onRequest(app)
--- src/PaymentPage/index.tsx
+++ src/PaymentPage/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import PaymentRequestForm from './PaymentButton'
+// import PaymentRequestForm from './PaymentButton'
+import CheckoutForms from './CheckoutForms'
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+const stripePromise = loadStripe('pk_test_51H4wHkAp105LrKrux4DQYvx5EL49Ewm98DuDWG9rftent74yxr22Dhf0BqlnaHIj0qquP7GjhNwXxFgiUHyqJdtN00XIvSGpMq');
 
 const PaymentPage = () => {
     return (
         <Elements stripe={stripePromise}>
             <h1>Hallo World</h1>
-            <PaymentRequestForm />
+            {/* <PaymentRequestForm /> */}
+            <CheckoutForms />
         </Elements>
     );
 };
